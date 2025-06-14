f7920de8d18b31936faa8cf3e5c782cde8be4b2c
Saurav <sauravsaurav0@gmail.com>
2025-06-14 09:21:58 +0000

    Create Starter file

---
+++ 1_ThreadCreation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+void taskA(){
+    cout<<"Task A Starts\n";
+    cout<<"Task A ends\n";
+}
+
+void taskB(){
+    cout<<"Task B Starts\n";
+    cout<<"Task B ends\n";
+}
+
+
+int main(){
+
+    return 0;
+}
