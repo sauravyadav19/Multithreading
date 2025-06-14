2f1aab98fa7554ab30f77e1c6dd80c9f7bb89377
Saurav <sauravsaurav0@gmail.com>
2025-06-14 11:11:50 +0000

    Create Starter file with To-do

---
+++ 2.1_.SingleVsParallel.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//**To-Do**
+//Implement a sequential simulation of downloading a file
+    // using sleep_for make the working thread sleep for 2 seconds
+//Clock the time
+
+int main(){
+
+    return 0;
+}
