14a3851fb60efaeb948a96b64084039f7944a109
Saurav <sauravsaurav0@gmail.com>
2025-06-14 09:49:27 +0000

    Create threads and add comments for future reference

--- 1_ThreadCreation.cpp
+++ 1_ThreadCreation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <thread> //contains all the thread related code
+using namespace std;
 
 void taskA(){
     cout<<"Task A Starts\n";
@@ -13,6 +14,16 @@ void taskB(){
 
 
 int main(){
+
+    thread t1(taskA); // create a thread and assign it to do taskA
+    thread t2(taskB); // create a thread and assign it to do taskB
+
+    //these two threads are running in parallel
+
+    // join() is always called from the perspective of the thread that created the other threads
+    //here "main" thread is manager for both t1 and t2
+    t1.join();
+    t2.join();
 
     return 0;
 }
