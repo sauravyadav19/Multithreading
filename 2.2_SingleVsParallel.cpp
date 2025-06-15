0a424a91bc87b063bf8ae85b7591ebc84a2e0b4c
Saurav <sauravsaurav0@gmail.com>
2025-06-15 11:29:07 +0000

    (2.2) Implement the Strcuture

--- 2.2_SingleVsParallel.cpp
+++ 2.2_SingleVsParallel.cpp
@@ -2,6 +2,7 @@
 //For sequential version of this refer to 2.1_SingleVsParallel.cpp
 #include<iostream>
 #include<thread>
+#include<vector>
 using namespace std;
 
 void downloadFile(int fileNumber){
@@ -15,8 +16,24 @@ int main(){
     auto startTime = chrono::high_resolution_clock::now();
     //Implement the thread-ed code
         //create multiple threads using vector
+    vector<thread> threads;
         //assing each of thread with task of dowloading a file
+    for (int i = 1; i<=5; i++){
+        //creating 5 threads and asinging each thread the task of running downloadFile
+        //the constructor of thread looks like this:
+        //std::thread(Callable&& f, Args&&... args);
+        //so whatever argument we want to pass to the function that we are passing to thread and  we want to give its
+        //parameter as an arugment meaning downloadfile(i) => thread(download,i)
+        threads.emplace_back(downloadFile,i);
+        // the above line of code is just a shorter and more memory efficient code of following:
+            //std::thread t(download_file, i);
+            //threads.push_back(std::move(t));
+
+    }
         // join them
+    for(auto&t:threads){
+        t.join();
+    }
     auto endTime = chrono::high_resolution_clock::now();
     auto totalDuration= chrono::duration_cast<chrono::seconds>(endTime - startTime).count();
     cout<<"The total time Taken : " << totalDuration <<" seconds" <<endl;
